a0807a8693b977f7070c2e187ca16edef11451cf
mariyamnavila <bibimariyamnavila@gmail.com>
2025-09-13 22:10:05 +0600

    added whole navbar and left,right sidebar

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,5 +1,8 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import LeftNavbar from "../components/layout-components/LeftNavbar";
+import RightNavbar from "../components/layout-components/RightNavbar";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
     return (
@@ -10,11 +13,13 @@ const HomeLayout = () => {
                     <LatestNews></LatestNews>
                 </section>
             </header>
-            <nav>
-
+            <nav className="w-11/12 mx-auto py-2">
+                <Navbar></Navbar>
             </nav>
-            <main>
-
+            <main className="w-11/12 mx-auto pt-5 grid grid-cols-12 gap-3">
+                <aside className="col-span-3"><LeftNavbar></LeftNavbar></aside>
+                <section className="col-span-6">Main</section>
+                <aside className="col-span-3"><RightNavbar></RightNavbar></aside>
             </main>
         </div>
     );
